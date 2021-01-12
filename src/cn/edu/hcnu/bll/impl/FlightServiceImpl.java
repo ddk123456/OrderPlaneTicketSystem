c60bbdfd7e72e536447ea3dc12bf59653a578301
liwei <7274561@qq.com>
2021-01-12 15:11:15 +0800

    1、创建IFlightService接口 2、创建FlightServiceImpl类，并实现IFlightService接口

---
+++ src/cn/edu/hcnu/bll/impl/FlightServiceImpl.java
@@ -0,0 +1,38 @@
+package cn.edu.hcnu.bll.impl;
+
+import cn.edu.hcnu.bean.Flight;
+import cn.edu.hcnu.bll.IFlightService;
+
+import java.util.Set;
+
+public class FlightServiceImpl implements IFlightService {
+    @Override
+    public void insertFlight(Flight flight) {
+
+    }
+
+    @Override
+    public Set<Flight> getAllFlights() {
+        return null;
+    }
+
+    @Override
+    public Flight getFlightByDepartureTime(String departureTime) {
+        return null;
+    }
+
+    @Override
+    public Flight getFlightByDepartureAirPort(String departureAirPort) {
+        return null;
+    }
+
+    @Override
+    public Flight getFlightByDestinationAirPort(String destinationAirPort) {
+        return null;
+    }
+
+    @Override
+    public void updateFlight(Flight flight) {
+
+    }
+}
